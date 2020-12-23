a52e98ee33f22f440b7627b80b661b12a0a22218
Dmitry Zharikov <zdimon77@gmail.com>
2020-12-23 18:15:19 +0200

    commit from zdimon77@gmail.com ----

---
+++ mentor/3/task.py
@@ -0,0 +1,15 @@
+import random
+print('Угадай число')
+random_number = random.randint(1,5)
+print(random_number)
+user_number = input('Вводи число')
+
+
+list_diagnoses = ['1. Рак', '2. Инсульт']
+
+
+
+
+
+
+
