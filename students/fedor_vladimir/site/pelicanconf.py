bda39146cdadebdf0701c66aa4b6581762ab3587
Fedor Grechaniy <fedorgrechaniy@gmail.com>
2020-12-25 18:35:10 +0200

    add content

--- students/fedor_vladimir/site/pelicanconf.py
+++ students/fedor_vladimir/site/pelicanconf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'fedos'
-SITENAME = 'Test'
+AUTHOR = 'Fedos'
+SITENAME = 'Blog'
 SITEURL = ''
 
 PATH = 'content'
@@ -32,5 +32,3 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-THEME = "themes/clean-blog"
