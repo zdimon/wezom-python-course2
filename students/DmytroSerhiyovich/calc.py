6b386047b5975e416dd2c6c852af26a145f594db
Your Name <you@example.com>
2020-12-26 10:49:06 +0200

    calc

---
+++ students/DmytroSerhiyovich/calc.py
@@ -0,0 +1,14 @@
+user_number1 = input("Введите первое число")
+user_number2 = input("Введите второе число")
+functions = ['1. Сложение', '2. Вычетание', '3. Умножение', '4. Деление']
+print(functions)
+user_choice = input("Выберите действие")
+if user_choice = 1:
+    rezult = user_number1 + user_number2
+if user_choice = 2:
+    rezult = user_number1 - user_number2
+if user_choice = 3:
+    rezult = user_number1 * user_number2
+if user_choice = 4:
+    rezult = user_number1 / user_number2
+print(rezult)
