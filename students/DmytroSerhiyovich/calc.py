67ce011e8e160549d98f053f00e7e3be0ba77739
kreizzza <solen330@gmail.com>
2020-12-26 12:01:05 +0200

    calc

--- students/DmytroSerhiyovich/calc.py
+++ students/DmytroSerhiyovich/calc.py
@@ -3,13 +3,13 @@ user_number2 = input("Введите второе число")
 functions = ['1. Сложение', '2. Вычетание', '3. Умножение', '4. Деление']
 print(functions)
 user_choice = input("Выберите действие")
-if user_choice == 1:
-    rezult = add(user_number1, user_number2)
-if user_choice == 2:
-    rezult = subtract(user_number1, user_number2)
-if user_choice == 3:
-    rezult = multiply(user_number1, user_number2)
-if user_choice == 4:
-    rezult = devide(user_number1, user_number2)
+if int(user_choice) == int(1):
+    rezult = int(user_number1) + int(user_number2)
+if int(user_choice) == int(2):
+    rezult = int(user_number1) - int(user_number2)
+if int(user_choice) == int(3):
+    rezult = int(user_number1) * int(user_number2)
+if int(user_choice) == int(4):
+    rezult = int(user_number1) / int(user_number2)
 
 print(rezult)
--- students/DmytroSerhiyovich/calc2.py
+++ students/DmytroSerhiyovich/calc2.py
@@ -1,18 +1,12 @@
-# Program make a simple calculator
-
-# This function adds two numbers
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
     return x / y
 
