5935dd2b0fe2729f795058fd219016335c059fe7
YevhenYachmenov <George_V@ua.fm>
2020-12-26 13:53:39 +0200

    Add Animals

--- students/yevhen_alexandr/4/libs/utils.py
+++ students/yevhen_alexandr/4/libs/utils.py
@@ -8,3 +8,21 @@ def add(*args):
             return None
 
     return sum
+
+
+
+class Animal():
+    color = 'red'
+    def move(self):
+        print('Top top I am %s!!!' % self.color)
+
+    def jump(self):
+        print('I am jumping')
+
+class Dog(Animal):
+    def say(self):
+        print('Gav gav')
+
+class Cat(Animal):
+    def say(self):
+        print('Miauuuu')
