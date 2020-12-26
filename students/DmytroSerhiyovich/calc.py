f273ae1bb87ba22f96c317e27c948034d3630de9
kreizzza <solen330@gmail.com>
2020-12-26 12:07:58 +0200

    calc

--- students/DmytroSerhiyovich/calc.py
+++ students/DmytroSerhiyovich/calc.py
@@ -1,15 +1,15 @@
-user_number1 = input("Введите первое число")
-user_number2 = input("Введите второе число")
+x = input("Введите первое число")
+y = input("Введите второе число")
 functions = ['1. Сложение', '2. Вычетание', '3. Умножение', '4. Деление']
 print(functions)
 user_choice = input("Выберите действие")
 if int(user_choice) == int(1):
-    rezult = int(user_number1) + int(user_number2)
+    rezult = int(x) + int(y)
 if int(user_choice) == int(2):
-    rezult = int(user_number1) - int(user_number2)
+    rezult = int(x) - int(y)
 if int(user_choice) == int(3):
-    rezult = int(user_number1) * int(user_number2)
+    rezult = int(x) * int(y)
 if int(user_choice) == int(4):
-    rezult = int(user_number1) / int(user_number2)
+    rezult = int(x) / int(y)
 
 print(rezult)
