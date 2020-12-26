a755020acd6db572c5e4ae2f508582840f50f348
YevhenYachmenov <George_V@ua.fm>
2020-12-26 12:38:23 +0200

    Add 4 start.py

---
+++ students/yevhen_alexandr/4/start.py
@@ -0,0 +1,14 @@
+name = 'Dima,Fedor,Yeugen,Ilya'
+
+name_2 = name.replace(',', '-')
+
+print(name_2)
+
+name_arr = name.split(',')
+
+name_3 = []
+for i, item in enumerate(name.split(',')):
+    name_3.append(f'{i + 1}.{item};')
+
+name_3 = ''.join(name_3)
+print(name_3)
