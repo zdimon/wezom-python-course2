7b527b9d2e31df15250d7cb5b3cc4586373a296f
Dmitry Zharikov <zdimon77@gmail.com>
2021-01-06 19:22:49 +0200

    commit from zdimon77@gmail.com ----

---
+++ mentor/5/bd.py
@@ -0,0 +1,16 @@
+
+import json
+
+s = json.loads(str)
+s = json.dumps(obj)
+
+def read_bd():
+    pass
+
+def write_bd():
+    pass
+
+def add_user(message):
+    data = read_bd()
+    data.append({...})
+    write_bd()
