2700091ca349ebccb02adbb954f527c2ba2e1e0e
Atlanfa <iliatalgar@gmail.com>
2021-01-13 18:02:52 +0200

    update

--- students/Ilia Evgenievich/botstart.py
+++ students/Ilia Evgenievich/botstart.py
@@ -11,15 +11,22 @@ def start_message(message):
     symb = random.choice(sym)
     num = random.randrange(6, 11)
     card =f'{num}-{symb}.png'
-    bot.send_message(message.chat.id, "Happy new year!!! /start")
+    bot.send_message(message.chat.id, "Hello, you are just typed /start \n typing /start again will reset your user score")
     bot.send_photo(chat_id=message.chat.id, photo=open('images/%s' % card, 'rb'))
+    #print(dir(message.chat))
+    #print(message)
     file = open('bd.json', 'r')
     s = file.read()
     file.close()
-    file = open('bd.json', 'w')
     dictionary = json.loads(s)
-    dictionary['usres'].append(message.chat.id) 
+    is_single = 0
+    for index in range(len(dictionary['users'])):
+        if dictionary['users'][index]['chat_id'] == f"{message.chat.id}":
+            is_single += 1
+    if is_single == 0:
+        dictionary['users'].append({"username": f"@{message.chat.username}", "chat_id": f"{message.chat.id}", "score": 0})
     str_dict = json.dumps(dictionary)
+    file = open('bd.json', 'w')
     file.write(str_dict)
     file.close()
 
@@ -31,11 +38,11 @@ def gues_number_game_start(message):
     file = open('bd.json', 'r')
     s = file.read()
     file.close()
-    file = open('bd.json', 'w')
     dictionary = json.loads(s)
     dictionary['random_number'] = num
     dictionary['game'] = 1
     str_dict = json.dumps(dictionary)
+    file = open('bd.json', 'w')
     file.write(str_dict)
     file.close()
     #file_num = open(r'random_num.txt', 'w')
@@ -44,6 +51,17 @@ def gues_number_game_start(message):
     #file_num.write(f'{num}')
     #file_num.close()
     #file_game.close()
+
+@bot.message_handler(commands=['score'])
+def display_score(message):
+    file = open('bd.json', 'r')
+    s = file.read()
+    file.close()
+    dictionary = json.loads(s)
+    scores = ""
+    for index in range(len(dictionary['users'])):
+        scores = scores + f"{dictionary['users'][index]['username']} - {dictionary['users'][index]['score']}\n"
+    bot.send_message(message.chat.id, f"{scores}")
 
 @bot.message_handler()
 def gues_number_game(message):
@@ -68,17 +86,24 @@ def gues_number_game(message):
             file = open('bd.json', 'r')
             s = file.read()
             file.close()
+            dictionary = json.loads(s)
+            dictionary['game'] = 0
+            for index in range(len(dictionary['users'])):
+                if dictionary['users'][index]['chat_id'] == f"{message.chat.id}":
+                    for index_1 in range(len(dictionary['users'])):
+                        bot.send_message(int(dictionary['users'][index_1]['chat_id']), f"user {dictionary['users'][index]['username']} scored in game")
+                    score = int(dictionary['users'][index]['score']) + 1
+                    dictionary['users'][index]['score'] = f"{score}"
+            str_dict = json.dumps(dictionary)
             with open('bd.json', 'w') as file:
-                dictionary = json.loads(s)
-                dictionary['game'] = 0
-                str_dict = json.dumps(dictionary)
                 file.write(str_dict)
             #file_game = open(r'game.txt', 'w+')
             #file_game.write('0')
             #file_game.close()
-        elif message.text != f'{num}':
-            bot.send_message(message.chat.id, 'you do not guesed the number, try again')
-    
+        elif int(message.text) > int(f'{num}'):
+            bot.send_message(message.chat.id, 'you number is bigger then hidden, try again')
+        elif int(message.text) < int(f'{num}'):
+            bot.send_message(message.chat.id, 'you number is lower then hidden, try again')
 
 
 print(BOT_KEY)
