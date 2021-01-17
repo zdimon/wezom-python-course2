5eaf20fc61d5e175d5db347109e7dff319caf72a
YevhenYachmenov <George_V@ua.fm>
2021-01-17 21:54:01 +0200

    Add imaginarium project

---
+++ students/yevhen_alexandr/imaginarium/game/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'game/index.html')
