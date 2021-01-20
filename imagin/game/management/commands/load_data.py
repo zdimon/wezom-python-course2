5aa90d28c398560183669089e7a170fefa8d800b
Dmitry Zharikov <zdimon77@gmail.com>
2021-01-20 19:02:21 +0200

    commit from zdimon77@gmail.com ----

--- imagin/game/management/commands/load_data.py
+++ imagin/game/management/commands/load_data.py
@@ -5,3 +5,17 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         print('Load data')
+        for i in range(1,3):
+            page = Page()
+            page.title = f'Page number {i}'
+            page.content = f'Page content {i}'
+            page.save()
+            image = Image()
+            image.title = f'Image {i}'
+            image.page = page
+            image.save()
+            print(f'Creating page {i}')
+
+        # image = Image()
+        # page = Page()
+        # image.page = page
