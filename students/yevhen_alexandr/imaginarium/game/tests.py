700785a36252d5a5b798ef5d1980416b86435aab
YevhenYachmenov <George_V@ua.fm>
2021-01-27 19:04:21 +0200

    Add home page test

--- students/yevhen_alexandr/imaginarium/game/tests.py
+++ students/yevhen_alexandr/imaginarium/game/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class homePageTestCase(TestCase):
+    def test_home_page_status_code(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+    #def test_home_page_images(self):
+
